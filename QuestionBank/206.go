eb1910440abf44c26fd16823df70fd3103b69122
灿灿 <1048042379@qq.com>
2019-06-25 17:15:21 +0800

    [upd] 第206题反转链表

--- QuestionBank/206.go
+++ QuestionBank/206.go
@@ -1,19 +1,21 @@
-// @Time     : 2019/4/29 17:13
-// @Author   : cancan
-// @File     : 206.go
-// @Function : 反转链表
+/*
+ * @Time     : 2019/4/29 17:13
+ * @Author   : cancan
+ * @File     : 206.go
+ * @Function : 反转链表
+ */
 
 /*
-Question:
-反转一个单链表。
-
-Example:
-输入: 1->2->3->4->5->NULL
-输出: 5->4->3->2->1->NULL
-
-Follow up:
-你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
-*/
+ * Question:
+ * 反转一个单链表。
+ *
+ * Example:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ * Follow up:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
 
 package QuestionBank
 

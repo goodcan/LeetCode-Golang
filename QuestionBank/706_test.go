67b90067775b2043d28b93b0baa4dcb890220526
灿灿 <1048042379@qq.com>
2019-06-27 10:49:21 +0800

    [upd] 第706题设计哈希映射

--- QuestionBank/706_test.go
+++ QuestionBank/706_test.go
@@ -13,13 +13,11 @@ import (
 
 func Test_706(t *testing.T) {
 	tests := []struct {
-		Id      int
 		methods []string
 		params  [][]interface{}
 		ans     []interface{}
 	}{
 		{
-			0,
 			[]string{"MyHashMap", "Put", "Put", "Get", "Get", "Put", "Get", "Remove", "Get"},
 			[][]interface{}{{}, {1, 1}, {2, 2}, {1}, {3}, {2, 1}, {2}, {2}, {2}},
 			[]interface{}{nil, nil, nil, 1, -1, nil, 1, nil, -1},
@@ -29,13 +27,13 @@ func Test_706(t *testing.T) {
 	s2m := utils.NewString2Method()
 	var hashMap MyHashMap
 
-	for _, test := range tests {
+	for index, test := range tests {
 		for i, method := range test.methods {
 			if i == 0 {
 				hashMap = Constructor706()
 			} else {
 				if !s2m.Check(s2m.Run(&hashMap, method, test.params[i]...), test.ans[i]) {
-					t.Errorf("failure id %d", test.Id)
+					t.Errorf("failure id %d", index)
 				}
 			}
 		}

af2c21d489254a9618536dee9206287abddaea8c
灿灿 <1048042379@qq.com>
2019-07-09 18:29:14 +0800

    [upd] 第739题每日温度

--- QuestionBank/739_test.go
+++ QuestionBank/739_test.go
@@ -21,7 +21,7 @@ func Test_T39(t *testing.T) {
 
 	for _, test := range tests {
 		if !utils.IntSliceEqual(dailyTemperatures(test.T), test.ans) {
-			t.Errorf("failure T %v ans %v \n", test.T, test.ans)
+			t.Errorf("failure T %v ans %v", test.T, test.ans)
 		}
 	}
 

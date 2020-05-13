9fbbad18eb246caef2983b9efc9307c51a78b75b
cancan <1048042379@qq.com>
2020-05-13 23:51:25 +0800

    [upd] SliceEqual 支持多维数组判等

--- utils/slice.go
+++ utils/slice.go
@@ -30,8 +30,24 @@ func SliceEqual(a, b interface{}) bool {
 	for i := 0; i < v1.Len(); i++ {
 		e1 := v1.Index(i)
 		e2 := v2.Index(i)
-		if e1.Interface() != e2.Interface() {
-			return false
+		isSlice1 := false
+		isSlice2 := false
+		switch e1.Interface().(type) {
+		case []int:
+			isSlice1 = true
+		}
+		switch e2.Interface().(type) {
+		case []int:
+			isSlice2 = true
+		}
+		if isSlice1 && isSlice2 {
+			if !SliceEqual(e1.Interface(), e2.Interface()) {
+				return false
+			}
+		} else {
+			if e1.Interface() != e2.Interface() {
+				return false
+			}
 		}
 	}
 
